f554643733e3ba1ca295bf704ebc1a9ffad4d7f7
Wendler Eis <wendler@icasei.com.br>
2018-12-11 10:23:51 -0200

    fix: remove react-native-paper

--- src/pokemonStore.js
+++ src/pokemonStore.js
@@ -1,18 +1,18 @@
 export default [
   {
-    number: '1',
+    id: '1',
     name: 'Bulbasaur',
     photoUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
     type: 'grass'
   },
   {
-    number: '4',
+    id: '4',
     name: 'Charmander',
     photoUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
     type: 'fire'
   },
   {
-    number: '7',
+    id: '7',
     name: 'Squirtle',
     photoUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
     type: 'water'
